6c874fc088335d5eedc03bcdeed905b43138d7c3
jonasmuriboe <jmurib@gmail.com>
2015-11-11 12:59:53 +0100

    Carousel

--- js/door_status.js
+++ js/door_status.js
@@ -1,5 +1,5 @@
 function checkDoor(){
-	document.getElementById("content").innerHTML='<iframe type="text/html" style="display:none;" src="http://hackerspace.idi.ntnu.no/api/door-api" style=""></iframe>';
+	document.getElementById("content").innerHTML='<iframe type="text/html" style="display:none;" src="127.0.0.1:5000/api/door" style=""></iframe>';
 	document.getElementsByTagName("iframe")[0].addEventListener("load", function(){
 		var a = document.getElementsByTagName("iframe")[0];
 		var b = JSON.parse(a.contentDocument.getElementsByTagName("pre")[0].innerHTML);
--- js/opening_bar.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $("#header").click(function() {
-    if($("body").hasClass("open")) {
-        $("body").removeClass("open closed").addClass("closed");
-    } else {
-        $("body").removeClass("open closed").addClass("open");
-    } 
-      });
-  });
